fcdcb528c29122f8482b0ee9d045540c0d6c5b82
thanhnam <nguyenthanhnamnv996@gmail.com>
2024-02-19 15:15:10 +0700

    first commit

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Deploy Me!</h1><style>body { display: flex; align-items: center; justify-content: center; height 100vh; }'
+    return '<h1>Deploy Me!</h1><style>body { display: flex; align-items: center; justify-content: center; height 100vh; }</style>'
